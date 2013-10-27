c3c229a510c1887a4628c3adc4860c1d14b3de35
wtarr <william.projects.2012@gmail.com>
2013-10-27 12:40:53 +0000

    Refactoring marching square class

--- MarchingSquare.py
+++ MarchingSquare.py
@@ -1,70 +1,99 @@
 __author__ = 'William'
 import Image
 
-im = Image.open("sprite.bmp")
-inew = Image.new("P", (16, 16))
-
-wnew, hnew = inew.size
-
-pixelsnew = inew.load()
-
-for x in range(wnew):
-    for y in range(hnew):
-        pixelsnew[x, y] = 255
-
-pixels = im.load()
-
-w, h = im.size
-
-x = 0
-y = 10
 white = 255
 black = 0
-path = []
 
-skip = 0
+def loadImage():
+    im = Image.open("sprite.bmp")
+    w, h = im.size
+    return im, w, h
 
-while True:
-    x0p = pixels[x, y]
-    y0p = pixels[x + 1, y]
-    x1p = pixels[x, y + 1]
-    y1p = pixels[x + 1, y + 1]
 
-    if (x0p is white and y0p is white and x1p is white and y1p is white) or \
+def createBlankImage():
+    imgNew = Image.new("P", (16, 16))
+    return imgNew
+
+def printName(name):
+    print name
+
+def marchingCube(im, w, h, imgNew):
+    pixels = im.load()
+    pixelsnew = imgNew.load()
+    x = 0
+    y = 10
+
+    skip = 0
+
+    while True:
+        x0p = pixels[x, y]
+        y0p = pixels[x + 1, y]
+        x1p = pixels[x, y + 1]
+        y1p = pixels[x + 1, y + 1]
+
+        if isALeftState(x0p, y0p, x1p, y1p):
+            if x+2 < w:
+                x += 1  # go right
+            pixelsnew[x, y] = 127
+        elif isARightState(x0p, y0p, x1p, y1p):
+            if x-1 >= 0:
+                x -= 1  # go left
+            pixelsnew[x, y] = 127
+        elif isAUpState(x0p, y0p, x1p, y1p):
+            if y - 1 >= 0:
+                y -= 1  # go up
+            pixelsnew[x, y] = 127
+        elif isADownState(x0p, y1p, x1p, y1p):
+            if y + 2 < h:
+                y += 1  # go down
+            pixelsnew[x, y] = 127
+
+        if skip > 1000 and pixelsnew[x, y] is 127:
+            break
+        skip+=1
+    imgNew.save('out.bmp')
+
+
+def isALeftState(x0p, y0p, x1p, y1p):
+            if (x0p is white and y0p is white and x1p is white and y1p is white) or \
             (x0p is black and y0p is black and x1p is white and y1p is white) or \
             (x0p is black and y0p is black and x1p is black and y1p is white) or \
             (x0p is white and y0p is black and x1p is white and y1p is white):
-        if x+2 < w:
-            x += 1  # go right
-        pixelsnew[x, y] = 127
-    elif (x0p is white and y0p is white and x1p is black and y1p is black) or \
-            (x0p is white and y0p is black and x1p is black and y1p is black) or \
-            (x0p is white and y0p is black and x1p is black and y1p is white) or \
-            (x0p is white and y0p is white and x1p is black and y1p is white):
-        if x-1 >= 0:
-            x -= 1  # go left
-        pixelsnew[x, y] = 127
-    elif (x0p is black and y0p is white and x1p is white and y1p is white) or \
-            (x0p is black and y0p is white and x1p is black and y1p is black) or \
-            (x0p is black and y0p is white and x1p is black and y1p is white) or \
-            (x0p is black and y0p is white and x1p is white and y1p is black):
-        if y - 1 >= 0:
-            y -= 1  # go up
-        pixelsnew[x, y] = 127
-    elif (x0p is white and y0p is black and x1p is white and y1p is black) or \
-            (x0p is white and y0p is white and x1p is white and y1p is black) or \
-            (x0p is black and y0p is black and x1p is white and y1p is black):
-        if y + 2 < h:
-            y += 1  # go down
-        pixelsnew[x, y] = 127
+                return True
+            else:
+                return False
 
-    if skip > 1000:
-        if pixelsnew[x, y] is 127:
-            break
-    skip+=1
-inew.save('out.bmp')
+def isARightState(x0p, y0p, x1p, y1p):
+    if (x0p is white and y0p is white and x1p is black and y1p is black) or \
+       (x0p is white and y0p is black and x1p is black and y1p is black) or \
+       (x0p is white and y0p is black and x1p is black and y1p is white) or \
+       (x0p is white and y0p is white and x1p is black and y1p is white):
+            return True
+    else:
+            return False
+
+def isAUpState(x0p, y0p, x1p, y1p):
+    if (x0p is black and y0p is white and x1p is white and y1p is white) or \
+        (x0p is black and y0p is white and x1p is black and y1p is black) or \
+        (x0p is black and y0p is white and x1p is black and y1p is white) or \
+        (x0p is black and y0p is white and x1p is white and y1p is black):
+            return True
+    else:
+            return False
+
+def isADownState(x0p, y0p, x1p, y1p):
+    if (x0p is white and y0p is black and x1p is white and y1p is black) or \
+        (x0p is white and y0p is white and x1p is white and y1p is black) or \
+        (x0p is black and y0p is black and x1p is white and y1p is black):
+            return True
+    else:
+            return False
+
+def main():
+    im, w, h = loadImage()
+    imgNew = createBlankImage()
+    marchingCube(im, w, h, imgNew)
 
 
-
-
+if __name__ == "__main__":main()
 
