83bf1b8650e4db674526aa652889808fa465cd46
wtarr <william.projects.2012@gmail.com>
2013-11-06 23:45:25 +0000

    updates to mar square

--- MarchingSquare.py
+++ MarchingSquare.py
@@ -3,15 +3,20 @@ import Image
 
 white = 255
 black = 0
+timeout = 10000
 
 def loadImage():
-    im = Image.open("sprite.bmp")
+    im = Image.open("large.bmp")
     w, h = im.size
     return im, w, h
 
 
-def createBlankImage():
-    imgNew = Image.new("P", (16, 16))
+def createBlankImage(w, h):
+    imgNew = Image.new("P", (w, h))
+    pix = imgNew.load()
+    for x in range(w):
+        for y in range(h):
+            pix[x, y] = white
     return imgNew
 
 
@@ -19,7 +24,7 @@ def marchingSquare(im, w, h, imgNew):
     pixels = im.load()
     pixelsnew = imgNew.load()
     x = 0
-    y = 10
+    y = 250
 
     skip = 0
 
@@ -32,21 +37,21 @@ def marchingSquare(im, w, h, imgNew):
         if isALeftState(x0p, y0p, x1p, y1p):
             if x+2 < w:
                 x += 1  # go right
-            pixelsnew[x, y] = 127
+            pixelsnew[x, y] = black
         elif isARightState(x0p, y0p, x1p, y1p):
             if x-1 >= 0:
                 x -= 1  # go left
-            pixelsnew[x, y] = 127
+            pixelsnew[x, y] = black
         elif isAUpState(x0p, y0p, x1p, y1p):
             if y - 1 >= 0:
                 y -= 1  # go up
-            pixelsnew[x, y] = 127
+            pixelsnew[x, y] = black
         elif isADownState(x0p, y1p, x1p, y1p):
             if y + 2 < h:
                 y += 1  # go down
-            pixelsnew[x, y] = 127
+            pixelsnew[x, y] = black
 
-        if skip > 1000 and pixelsnew[x, y] is 127:
+        if skip > timeout:
             break
         skip+=1
     imgNew.save('out.bmp')
@@ -89,7 +94,7 @@ def isADownState(x0p, y0p, x1p, y1p):
 
 def main():
     im, w, h = loadImage()
-    imgNew = createBlankImage()
+    imgNew = createBlankImage(w, h)
     marchingSquare(im, w, h, imgNew)
 
 
