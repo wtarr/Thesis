276021021e0730c6423941252e61c25aad97f396
Will <william.tarrant@students.ittralee.ie>
2014-01-23 22:26:03 +0000

    Add light factory, working on automating object creation, writing unit tests and storing voxel state

--- tests/test.js
+++ tests/test.js
@@ -30,3 +30,38 @@ test("Test Sphere collision", function(){
 
 
 });
+
+test("Spotlight creation via Light Factory with options set", function() {
+
+    var lightFactory = new LightFactory();
+    var spotlight = lightFactory.createLight(
+        {
+            lightType: "spotlight",
+            position: new THREE.Vector3(0, 0, 10),
+            shouldCastShadow: true,
+            target: new THREE.Vector3(10, 15, 20)
+        }
+    )
+
+    ok(spotlight instanceof THREE.SpotLight, "Is an instance of spotlight");
+    ok((spotlight.position.x === 0 && spotlight.position.y === 0 && spotlight.position.z === 10) , "Position set correctly");
+    ok(spotlight.castShadow === true, "cast should is set true");
+    ok((spotlight.target.x === 10, spotlight.target.y === 15, spotlight.target.z === 20), "Target set correctly");
+
+
+});
+
+test("Spotlight creation via Light Factory with defaults", function() {
+   var lightFactory = new LightFactory();
+    var spotlight = lightFactory.createLight(
+        {
+            lightType: "spotlight"
+        }
+    )
+
+    ok(spotlight instanceof THREE.SpotLight, "Is an instance of spotlight");
+    ok((spotlight.position.x === 0 && spotlight.position.y === 0 && spotlight.position.z === 0) , "Default position set correctly");
+    ok(spotlight.castShadow === false, "default set of cast shadow should be set to false");
+    ok((spotlight.target instanceof THREE.Object3D), "Default target set correctly");
+
+});
